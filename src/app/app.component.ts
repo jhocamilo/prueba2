82fb4be0e184b624baf19797162c00bf2baebd78
jhocamilo <johangiraldo@unicomfacauca.edu.co>
2021-08-10 17:16:23 -0500

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { persona} from './model/persona';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'prueba';
+  personaArray: persona[] = [
+    {identificacion:1, nombre:"Alejo", apellido:"ospina", sexo:"Masculino", RH:"A-" },
+    {identificacion:2, nombre:"Alexa", apellido:"ortega", sexo:"femenino", RH:"A+"},
+    {identificacion:1, nombre:"rodrigo", apellido:"marquez", sexo:"Masculino", RH:"A-"}
+  ];
+
+  selectedpersona: persona = new persona();
+
+  open(persona: persona){
+    this.selectedpersona=persona;
+
+  }
+
+  eliminar(){
+    this.personaArray = this.personaArray.filter(x => x != this.selectedpersona);
+    this.selectedpersona=new persona;
+  }
 }
---
+++ src/app/model/persona.ts
@@ -0,0 +1,7 @@
+export class  persona{
+    identificacion:number=0;
+    nombre:string="";
+    apellido:string="";
+    sexo:string="";
+    RH:string="";
+    }
